cac7b3130f457027fe59a88f034444c5e3bad669
hugo <hugo@datasentinel.io>
2026-03-13 10:57:53 +0100

    add test for manual vacuum

--- sql/manualanalyze.sql
+++ sql/manualanalyze.sql
@@ -2,13 +2,12 @@
 SELECT ds_autoanalyze_activity_reset();
 
 CREATE TABLE test_mv (id int);
--- INSERT INTO test_mv SELECT generate_series(1, 5);
--- DELETE FROM test_mv;
 
 -- Disable force_verbose so verbosity is controlled explicitly.
-SET pg_datasentinel.maintenance_force_verbose = off;
+SET pg_datasentinel.maintenance_force_verbose = off;                                                                                                                                                                                                                                                                                                        
 
--- Test 2: ANALYZE VERBOSE should be captured in ds_autoanalyze_activity.
+
+-- Test 1: only ANALYZE VERBOSE should be captured in ds_autoanalyze_activity.
 ANALYZE test_mv;
 
 SELECT datname, schemaname, relname, message AS manual_analyze_captured
@@ -23,10 +22,11 @@ WHERE message ~* 'test_mv';
 
 SELECT ds_autoanalyze_activity_reset();
 
--- Test 4: With maintenance_force_verbose = on, plain ANALYZE is captured.
-SET pg_datasentinel.maintenance_force_verbose = on;
 
--- Test 2: ANALYZE VERBOSE should be captured in ds_autoanalyze_activity.
+-- Test 2: With maintenance_force_verbose = on, plain ANALYZE is captured.
+SET pg_datasentinel.maintenance_force_verbose = on;                                                                                                                                                                                                                                                                                                        
+
+
 ANALYZE test_mv;
 
 SELECT datname, schemaname, relname, message AS manual_analyze_captured
@@ -40,5 +40,3 @@ ANALYZE VERBOSE test_mv;
 SELECT datname, schemaname, relname, message AS manual_analyze_captured
 FROM ds_autoanalyze_activity
 WHERE message ~* 'test_mv';
-
-DROP TABLE test_mv;
